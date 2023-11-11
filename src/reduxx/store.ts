3bb33e3e38e3489d7146d5ca5bcdda08f7ec4e13
vaniafed <ztankist@gmail.com>
2023-11-11 16:04:37 +0300

    Feature store: store books in local storage

--- src/reduxx/store.ts
+++ src/reduxx/store.ts
@@ -1,13 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { getBooksFromLocalStorage } from 'utils/get-books-from-localstorage';
+import { saveBooksToLocalStorage } from 'utils/save-books-to-localstorage';
+
 import bookReducer from './slices/book/slice';
 
 export const store = configureStore({
 	reducer: {
 		book: bookReducer,
 	},
-
+	preloadedState: {
+		book: getBooksFromLocalStorage(),
+	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+
+store.subscribe(() => {
+	saveBooksToLocalStorage(store.getState().book);
 });
 
 export type AppDispatch = typeof store.dispatch;
--- src/utils/filter-books.ts
+++ src/utils/filter-books.ts
@@ -2,6 +2,8 @@ import type { Book } from 'reduxx/slices/book/types';
 
 export const filterBooks = (books: Book[], filter: string): Book[] => {
 	if (!books) return [];
+	if (!filter) return books;
+
 	return books.filter(
 		({ title, author }) =>
 			title.toLowerCase().includes(filter.toLowerCase()) || author.toLowerCase().includes(filter.toLowerCase())
